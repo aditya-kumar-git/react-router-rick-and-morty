770140087e764f038865ac7f6f3bf60c58606dfe
Aditya Kumar <adityakumar@Aditya-Kumars-Mac.local>
2020-07-17 14:39:30 +0530

    ready to push

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -4,68 +4,66 @@ import { Link } from "react-router-dom"
 import gsap from "gsap"
 
 class Characters extends React.Component {
-	componentDidMount() {
-		gsap.to(".charLink", { duration: 0, color: "coral" })
-		gsap.to(".homeLink", { duration: 0, color: "white" })
-		gsap.to(".epiLink", { duration: 0, color: "white" })
+  componentDidMount() {
+    gsap.to(".charLink", { duration: 0, color: "coral" })
+    gsap.to(".homeLink", { duration: 0, color: "white" })
+    gsap.to(".epiLink", { duration: 0, color: "white" })
 
-		gsap.from(".characterBlock", {
-			duration: 0.5,
-			opacity: 0,
-			y: "30%",
-			stagger: 0.1,
-		})
-	}
-	renderingBlocks = () => {
-		if (this.props.character[0]) {
-			return this.props.character.map((ele) => {
-				return (
-					<Link key={ele.id} to={`/characters/${ele.id}`}>
-						<div className="characterBlock">
-							<div className="cbContainer">
-								<div
-									style={{ backgroundImage: `url("${ele.image}")` }}
-									className="cbcImage"
-								>
-									<div className="cbciName">{ele.name}</div>
-								</div>
-								<div className="cbcInfo">
-									<div className="ci">
-										<div>STATUS</div>
-										<div className="ciData">{ele.status}</div>
-									</div>
-									<div className="ci">
-										<div>SPECIES</div>
-										<div className="ciData">{ele.species}</div>
-									</div>
-									<div className="ci">
-										<div>GENDER</div>
-										<div className="ciData">{ele.gender}</div>
-									</div>
-									<div className="ci">
-										<div>ORIGIN</div>
-										<div className="ciData">{ele.origin.name}</div>
-									</div>
-									<div className="ci">
-										<div>LOCATION</div>
-										<div className="ciData">{ele.location.name}</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</Link>
-				)
-			})
-		} else {
-			return <div>Loading...</div>
-		}
-	}
+    gsap.from(".characterBlock", {
+      duration: 0.5,
+      opacity: 0,
+      y: "30%",
+      stagger: 0.1,
+    })
+  }
+  renderingBlocks = () => {
+    if (this.props.character[0]) {
+      return this.props.character.map((ele) => {
+        return (
+          <div className="characterBlock">
+            <div className="cbContainer">
+              <div
+                style={{ backgroundImage: `url("${ele.image}")` }}
+                className="cbcImage"
+              >
+                <div className="cbciName">{ele.name}</div>
+              </div>
+              <div className="cbcInfo">
+                <div className="ci">
+                  <div>STATUS</div>
+                  <div className="ciData">{ele.status}</div>
+                </div>
+                <div className="ci">
+                  <div>SPECIES</div>
+                  <div className="ciData">{ele.species}</div>
+                </div>
+                <div className="ci">
+                  <div>GENDER</div>
+                  <div className="ciData">{ele.gender}</div>
+                </div>
+                <div className="ci">
+                  <div>ORIGIN</div>
+                  <div className="ciData">{ele.origin.name}</div>
+                </div>
+                <div className="ci">
+                  <div>LOCATION</div>
+                  <div className="ciData">{ele.location.name}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })
+    } else {
+      return <div>Loading...</div>
+    }
+  }
 
-	render() {
-		return <div className="characters">{this.renderingBlocks()}</div>
-	}
+  render() {
+    return <div className="characters">{this.renderingBlocks()}</div>
+  }
 }
 var mapStateToProps = (state) => {
-	return state
+  return state
 }
 export default connect(mapStateToProps)(Characters)
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,11 +2,5 @@ import { createStore, compose, applyMiddleware } from "redux"
 import allReducers from "./Reducers"
 import thunk from "redux-thunk"
 
-var myStore = createStore(
-	allReducers,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-)
+var myStore = createStore(allReducers, compose(applyMiddleware(thunk)))
 export default myStore
